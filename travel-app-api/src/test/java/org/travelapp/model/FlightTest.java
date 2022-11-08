13ff373a143da41cd4818655eb901cd6d0244c9f
ravynwhite <ravynwhite7@gmail.com>
2022-11-08 07:23:20 -0600

    initial flight test

--- travel-app-api/src/test/java/org/travelapp/model/FlightTest.java
+++ travel-app-api/src/test/java/org/travelapp/model/FlightTest.java
@@ -1,4 +1,60 @@
+package org.travelapp.model;
+
 import junit.framework.TestCase;
+
 public class FlightTest extends TestCase {
-  
+
+    public void testGetTimeZone() {
+    }
+
+    public void testSetTimeZone() {
+    }
+
+    public void testGetFlightArrival() {
+    }
+
+    public void testSetFlightArrival() {
+    }
+
+    public void testGetFlightDeparture() {
+    }
+
+    public void testSetFlightDeparture() {
+    }
+
+    public void testGetCityArrival() {
+    }
+
+    public void testSetCityArrival() {
+    }
+
+    public void testGetCityDeparture() {
+    }
+
+    public void testSetCityDeparture() {
+    }
+
+    public void testGetFlightLength() {
+    }
+
+    public void testSetFlightLength() {
+    }
+
+    public void testGetPlaneType() {
+    }
+
+    public void testSetPlaneType() {
+    }
+
+    public void testGetAirline() {
+    }
+
+    public void testSetAirline() {
+    }
+
+    public void testGetMaxPassengers() {
+    }
+
+    public void testSetMaxPassengers() {
+    }
 }
